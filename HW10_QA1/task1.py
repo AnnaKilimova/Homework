2ffcff2f68b45468130cfde806a8191581da3c8b
ann_kilimova <annkilimova85@gmail.com>
2022-12-04 23:17:41 +0200

    Displayed 5 stars on the screen

--- HW10_QA1/task1.py
+++ HW10_QA1/task1.py
@@ -3,14 +3,14 @@
 import random
 import string
 
-def randStr(s_r, length):
+def get_random_string(s_r, length):
     return ''.join(random.choice(s_r) for _ in range(length))
 
 string_random = string.ascii_uppercase + string.ascii_lowercase + string.digits
 
-print(randStr(string_random, length = 7))
-print(randStr(string_random, length = 10))
-print(randStr(string_random, length = 12))
+print(get_random_string(string_random, length = 7))
+print(get_random_string(string_random, length = 10))
+print(get_random_string(string_random, length = 12))
 
 # Не понимаю (закомментила ниже) почему когда я прописываю параметры как *args, **kwargs функция не работает?
 
