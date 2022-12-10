06c9c6b2a651bb5efa486962b633d2c7aa1adf83
kilimovaann <annkilimova85@gmail.com>
2022-12-10 22:18:39 +0000

    Displayed 5 stars on the screen

---
+++ HW11_Regular expressions/test.py
@@ -0,0 +1,15 @@
+text = "Hello, cocroach. And where it is? Who, can do it?! Or vice versa!? Yes, it's difficult... Claro.."
+
+import re
+def generate_sentence(text: str) -> str:
+    pattern = '[A-Z]\w+'
+    res2 = re.findall(pattern, text)
+
+    new_str = " ".join(res2).lower() + '.'
+
+    new_string = re.sub(r'^\w', new_str[:1].upper(), new_str)
+    return new_string
+
+print(generate_sentence(text))
+
+
